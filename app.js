a039da1e4e5d1160219c3064a8f2fdfee3366a71
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-01-15 23:05:41 +0530

    filter input added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const filterContainer = document.querySelector(".filters");
+
 const filters = {
   brigthness: {
     value: 100,
@@ -24,32 +26,69 @@ const filters = {
     unit: "%",
   },
   hueRotation: {
-    value: 100,
+    value: 0,
     min: 0,
-    max: 200,
-    unit: "%",
+    max: 360,
+    unit: "deg",
   },
   blur: {
     value: 0,
+    min: 0,
+    max: 20,
+    unit: "px",
   },
   greyScale: {
     value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
   },
   sepia: {
     value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
   },
   opacity: {
     value: 100,
+    min: 0,
+    max: 100,
+    unit: "%",
   },
   invert: {
     value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
   },
 };
+
 function inputBuilder(name, value, min, max, unit) {
   const div = document.createElement("div");
+  div.classList.add("filter");
+
+  const input = document.createElement("input");
+  input.type = "range";
+  input.value = value;
+  input.min = min;
+  input.max = max;
+  input.id = name;
+
   const p = document.createElement("p");
   p.textContent = name;
-  const input = document.createElement("input");
-  input.setAttribute("type", "range");
-  div.classList.add("filter");
+
+  div.appendChild(p);
+  div.appendChild(input);
+  return div;
 }
+Object.keys(filters).forEach((filter) => {
+  const filterElement = inputBuilder(
+    filter,
+    filters[filter].value,
+    filters[filter].min,
+    filters[filter].max,
+    filters[filter].unit
+  );
+
+  filterContainer.appendChild(filterElement);
+});
